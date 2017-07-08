8947a24b3ac28d2fd1d99c64f18e9234e1f40734
github username <github email address>
2017-07-08 11:56:33 -0400

    Implemented users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update, :show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to PinTrips! #{@user.user_name}"
+      #Maybe hange this path once we have a better page to redirect them to?
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password, :first_name, :last_name)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   has_many :trips, through: :events
   has_many :locations, through: :events
   has_many :images, through: :events
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :user_name, presence: true,
+                        uniqueness: {case_sensitive: false},
+                        length: {minimum:3, maximum: 25}
+
+  has_secure_password
 end
