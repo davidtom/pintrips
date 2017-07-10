3bc1ecc4f275a73455d06c53e490a0da505df5e7
github username <github email address>
2017-07-10 11:39:24 -0400

    Added validation for comments to make sure either trip, or event ID is present

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :trip
-  belongs_to :event
+  belongs_to :trip, optional: true
+  belongs_to :event, optional: true
+  validate :event_or_user
+
+
+  def event_or_user
+    if !self.trip && !self.event
+      #Add error to comment.
+      self.errors << "Must have either trip or event"
+    end
+  end
+
 end
