e9e8377eda6313104a7bbe163e6b8886ba21fc74
Charles Wooley <cwooley89@gmail.com>
2017-07-11 08:30:24 -0400

    Added method stubs and view pages for trips

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,42 @@
+class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update]
+  before_action :require_user, only:[:new, :edit, :update]
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:events)
+  end
+end
