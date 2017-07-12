4f9ff8141d6a6dccd951b3a595a4137b1fab791b
davidtom <davidjtomczyk@gmail.com>
2017-07-12 12:12:17 -0400

    refactor methods in trips_controller per jeff's feedback

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,13 +4,14 @@ class TripsController < ApplicationController
 
   def new
     @events  = [] # DOnt ask.
-    @orphan_events = get_orphan_events
+    @orphan_events = current_user.orphan_events
     @trip = Trip.new
   end
 
   def create
     trip = Trip.new(trip_params)
-    trip.user = current_user
+    byebug
+    current_user.trips << trip
     if trip.save
       flash[:success] = "Trip successfully created!"
       redirect_to user_path(current_user)
@@ -39,7 +40,7 @@ class TripsController < ApplicationController
       redirect_to request.referer
     else
       @events = @trip.events
-      @orphan_events = get_orphan_events
+      @orphan_events = current_user.orphan_events
       #store previous page so it can be linked back to after update
       session[:return_to] ||= request.referer
       end
@@ -69,7 +70,7 @@ class TripsController < ApplicationController
   end
 
   def friends
-    @trips = Trip.all.select { |trip| current_user.friends.include?(trip.user) }
+    @trips = current_user.friend_trips
   end
 
   private
@@ -80,10 +81,6 @@ class TripsController < ApplicationController
 
   def trip_params
     params.require(:trip).permit(:name, event_ids:[])
-  end
-
-  def get_orphan_events
-    current_user.events.select { |event| event.trip_id == nil }
   end
 
   def user_match?
