ea905237f79850e4c7ea971a4c37339a5fbed292
jeremylenz <jeremylenz@gmail.com>
2017-07-13 18:03:32 -0400

    removed comma on line 17 of user.rb that was breaking everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 
 class User < ApplicationRecord
   has_many :friendships, :dependent => :destroy
-  has_many :friends, through: :friendships,
+  has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", :dependent => :destroy
   has_many :inverse_friends, through: :inverse_friendships, source: :user, :dependent => :destroy
   has_many :comments, :dependent => :destroy
